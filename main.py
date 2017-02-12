05d3af91f06e800251aff914e3857b7dd3e12e6c
Eric Cline <ericcline@Erics-MacBook-Pro-2.local>
2017-02-11 19:50:42 -0600

    it works

--- main.py
+++ main.py
@@ -8,47 +8,78 @@ class Line(object):
     """Stores a pair of edge positions and can generate an appropriately
     sized string from them"""
 
+    EDGE = "!"
+
     def __init__(self, left, right):
-        self.lx self.ly = left
-        self.rx self.ry = right
+        self.lx = left
+        self.rx = right
+        self.y = curses.LINES - 1
+
+    def update(self):
+        self.y += -1
 
     def __str__(self):
-        return " " * (self.rx - self.lx)
+        return "{edge}{middle}{edge}".format(edge=self.EDGE, middle=" " * (self.rx - self.lx))
 
 
 class Course(object):
     """Generates and stores a screen's worth of edge positions"""
 
+    MIN_WIDTH = 6
+    MAX_WIDTH = 25
+
     def __init__(self):
-        self._width, self._height = curses.COLS, curses.LINES
+        self._width, self._height = curses.COLS - 1 , curses.LINES - 1
         self._course = queue.deque(maxlen=self._height)
+        self._left_edge = 5
+        self._right_edge = 15
         self._populate()
 
     def _populate(self):
         for i in range(self._height):
-            line = Line(self._gutter)
+            line = Line(self._left_edge, self._right_edge)
             self._course.append(line)
-            self._new_gutter()
+            self._update_edges()
+            self._update_lines()
 
     def evolve(self):
         self._course.popleft()
-        self._course.append(Line(self._gutter))  # append something
-        return self
+        self._update_edges()
+        self._update_lines()
+        self._course.append(Line(self._left_edge, self._right_edge))
+
+    def _update_lines(self):
+        [l.update() for l in self._course]
+
+    @property
+    def lines(self):
+        return self._course
 
     def get_line(self, index):
         return self._course[index]
 
-    def _new_gutter(self):
-        direction = random.choice((-1, 0, 1))
-        while not 1 < (self._gutter + direction) < self._width:
-            direction = random.choice((-1, 0, 1))
-        self._gutter += direction
+    def _update_edges(self):
+        left_choices = {
+            self.MIN_WIDTH: (-1, 0),
+            self.MAX_WIDTH: (0, 1)
+        }
+        right_choices = {
+            self.MIN_WIDTH: (0, 1),
+            self.MAX_WIDTH: (-1, 0)
+        }
+        current_distance = self._right_edge - self._left_edge
+        choices = left_choices.get(current_distance, (-1, 0, 1))
+        direction = random.choice(choices)
+        while not self._left_edge + direction > 1:
+            direction = random.choice(choices)
+        self._left_edge += direction
 
-    def __repr__(self):
-        return "\n".join(str(line) for line in self._course)
-
-    def __str__(self):
-        return "\n".join(str(line) for line in self._course)
+        current_distance = self._right_edge - self._left_edge
+        choices = right_choices.get(current_distance, (-1, 0, 1))
+        direction = random.choice(choices)
+        while not self._right_edge + direction < self._width:
+            direction = random.choice(choices)
+        self._right_edge += direction
 
 
 class Player(object):
@@ -65,7 +96,7 @@ class Player(object):
 
     def is_crashed(self, course):
         player_line = course.get_line(self.y)
-        if str(player_line)[self.x] == "!":
+        if self.x in (player_line.lx, player_line.rx):
             return True
         return False
 
@@ -81,8 +112,9 @@ def main(stdscr):
     stdscr.nodelay(True)
     course = Course()
     start_y = int(curses.LINES / 2)
-    start_x = course.get_line(start_y).left_edge + 5
+    start_x = course.get_line(start_y).lx + 5
     player = Player(start_x, start_y)
+    distance = 0
 
     current_time = time.time()
     while True:
@@ -92,14 +124,16 @@ def main(stdscr):
         if not player.is_crashed(course):
             new_time = time.time()
             if new_time - current_time > .1:
+                distance += 1
                 current_time = new_time
-                stdscr.addstr(0, 0, str(course.evolve()))
-                stdscr.clrtoeol()
+                course.evolve()
+                stdscr.clear()
+                [stdscr.addstr(line.y, line.lx, str(line)) for line in course.lines]
             player.update(key_down)
             stdscr.addch(player.y, player.x, str(player))
             stdscr.refresh()
         else:
-            stdscr.addstr(player.y, player.x + 1, "<------------ You died here!")
+            stdscr.addstr(player.y, player.x + 1, "<------------ You died here! But you made it {} feet!".format(distance))
             stdscr.refresh()
 
 
